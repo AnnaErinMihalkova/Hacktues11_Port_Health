a71a9b9c6c4d0468662461cfd051f74d57cff7f8
AnnaErinMihalkova <anna.erin05@gmail.com>
2025-03-20 23:05:23 +0200

    Main update

---
+++ app/src/main/java/com/example/port_health/RemindersActivity.java
@@ -0,0 +1,13 @@
+package com.example.port_health;
+import com.example.port_health.R;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RemindersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}
